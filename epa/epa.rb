6aada096722d05b63d1fa6db5bb7f0bac18c0254
Rodney Cobb <roacobb@gmail.com>
2014-05-06 12:29:33 -0400

    Inital commit_2

---
+++ epa/epa.rb
@@ -0,0 +1,12 @@
+# session = TembooSession.new("roacobb21", "APP_NAME", "APP_KEY")
+listReposByOrgChoreo = GitHub::ReposAPI::Repos::ListReposByOrg.new(session)
+
+# Get an InputSet object for the choreo
+listReposByOrgInputs = listReposByOrgChoreo.new_input_set()
+
+# Set inputs
+listReposByOrgInputs.set_AccessToken("d37fe5acbaa0d1fe05122fb9d42c5685987f6145");
+listReposByOrgInputs.set_Org("usepa");
+
+# Execute Choreo
+listReposByOrgResults = listReposByOrgChoreo.execute(listReposByOrgInputs)
